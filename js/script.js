72602a37aa0980f5e38c9a2659a8768343142a8d
ivan <example.gg.ru>
2020-10-05 21:30:37 +0200

    four commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
 'use strict';
-// let person = {
-//     name: 'john',
-//     age: 25,
-//     isMarried: false
+
+// switch (num) {
+//     case num < 49:
+//         console.log('Неверно');
+//         break;
+//     case num > 100:
+//         console.log('много');
+//         break;
+//     case num > 80:
+//         console.log('Все еще много');
+//         break;
+//     case 50:
+//         console.log('Верно');
+//         break;
+//     default:
+//         console.log('Что то пошло не так');
+//         break;
+// };
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
 // }
-// console.log(person['name']);
-// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-// console.log(arr[0]);
-// let answer = confirm('are you here?');
-// console.log(answer);
-// let answer = +prompt('are you here?', '24');
-// console.log(typeof(answer));
-// console.log(4 + +' - ')
-let incr = 10,
-    decr = 10;
-
-
-console.log(incr++);
-console.log(decr--);
-console.log(5 % 2);
-console.log('2' === 2);
-let isChecked = true,
-    isClose = false;
-console.log(isChecked || !isClose);
+let num = 50;
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
