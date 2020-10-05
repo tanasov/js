f8874b03f1273b361c246a6bb5466ad5daeca4c7
ivan <example.gg.ru>
2020-10-05 20:20:21 +0200

    it is first commit

---
+++ .bashrc
@@ -0,0 +1,3 @@
+# Set Git language to English
+#alias git='LANG=en_US git'
+alias git='LANG=en_GB git'
---
+++ js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+// let person = {
+//     name: 'john',
+//     age: 25,
+//     isMarried: false
+// }
+// console.log(person['name']);
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[0]);
+// let answer = confirm('are you here?');
+// console.log(answer);
+// let answer = +prompt('are you here?', '24');
+// console.log(typeof(answer));
+// console.log(4 + +' - ')
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5 % 2);
+console.log('2' === 2);
+let isChecked = true,
+    isClose = false;
+console.log(isChecked || !isClose);
