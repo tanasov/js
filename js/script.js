bc18684602f08ffd3d549f508816d55e392a22b4
Aleksandr <aleksandr@MacBook-Air-Aleksandr.local>
2020-10-11 18:49:26 +0200

    11

--- js/script.js
+++ js/script.js
@@ -8,28 +8,19 @@ const personalMovieDB = {
     privat: false
 };
 
+let num = 20;
 
-
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
 }
-if (personalMovieDB.count < 10) {
-    console.log('Просмотренно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+showFirstMessage('Hello world');
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
 }
-console.log(personalMovieDB);
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
