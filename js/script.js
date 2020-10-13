7b8c9aa77d8f138279834fc4e33db4e04f301600
Aleksandr <aleksandr@MacBook-Air-Aleksandr.local>
2020-10-13 18:48:08 +0200

    1

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ function calc(a, b) {
 console.log(calc(4, 3));
 console.log(calc(5, 6));
 console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log('Hello');
+};
+logger();
+const calc1 = (a, b) => a + b;
