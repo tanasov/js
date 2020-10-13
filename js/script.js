ea67297c48cb310123c883849428c6a9434e044d
Aleksandr <aleksandr@MacBook-Air-Aleksandr.local>
2020-10-13 19:08:31 +0200

    2222e

--- js/script.js
+++ js/script.js
@@ -1,30 +1,12 @@
 'use strict';
-let num = 20;
+const str = 'test';
+const arr = [1, 2, 3];
+console.log(str[2]);
+console.log(str.toUpperCase());
+console.log(arr.length);
 
-function showFirstMessage(text) {
-    console.log(text);
-    let num = 10;
-    console.log(num);
-}
-showFirstMessage('Hello world');
-console.log(num);
-
-function calc(a, b) {
-    return (a + b);
-}
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
-
-function ret() {
-    let num = 50;
-    return num;
-}
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function() {
-    console.log('Hello');
-};
-logger();
-const calc1 = (a, b) => a + b;
+const num = 12.2;
+console.log(Math.round(num));
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
